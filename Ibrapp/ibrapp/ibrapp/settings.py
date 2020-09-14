124b0e23ea2a3526ddc317df43df0f1e93b233ed
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2020-09-13 18:23:22 -0700

    Update settings.py

--- Ibrapp/ibrapp/ibrapp/settings.py
+++ Ibrapp/ibrapp/ibrapp/settings.py
@@ -129,10 +129,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home-main'
 LOGIN_URL = 'login'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_POST = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'craftingsite5665'
-EMAIL_HOST_PASSWORD = 'coldairs5665'
